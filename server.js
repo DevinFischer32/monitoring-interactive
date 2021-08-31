8f0c0e7b3b022f7db40002d4d5fcdce6c7c13de6
Devin Fischer <devinfischer32@gmail.com>
2021-08-31 12:35:29 -0600

    added json parse

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const Rollbar = require('rollbar')
+const { rootCertificates } = require('tls')
 
 let rollbar = new Rollbar({
     accessToken: '055ff42d2cb84f029a17e32eda8bcd4d',
@@ -12,21 +13,32 @@ const students = []
 const app = express()
 app.use(express.json())
 
+app.use('/style', express.static('./public/style.css'))
+
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname, '/public/index.html'))
     rollbar.info('html file served successfully')
 })
 
-app.post('/api/student',(req,res)=>{
+app.post('/api/student', (req, res)=>{
     let {name} = req.body
     name = name.trim()
-    
-    students.push(name)
-    
-    rollbar.log('Student added successfully', {author: 'Devin', type: 'manual entry' })
-    
-    res.status(200).send(students)
+
+    const index = students.findIndex(studentName=> studentName === name)
+
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('student added successfully',{author: 'Devin'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        res.status(400).send('must provide a name.')
+        rollbar.error('no name given')
+    } else {
+        res.status(400).send('that student already exists')
+        rollbar.error('student already exisits')
+    }
+
 })
 
 const port = process.env.PORT || 4545
